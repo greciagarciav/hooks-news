b51ba3e6d33aeaebbc7ec7d790919de272af6776
Grecia Villacorta <ggarciavillacort@tripadvisor.com>
2022-03-02 16:08:47 -0500

    data fetching

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+
+  const [results, setResults] = useState([])
+  const [query, setQuery] = useState(['react hooks'])
+
+  //useEffect(() => {
+  //  axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+  //        .then(response => {
+  //          console.log(response.data)
+  //          setResults(response.data.hits)
+  //        })
+  //}, [])
+
+  //useEffect(async() => {
+  //  const response = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+  //  setResults(response.data.hits)
+  //}, [])
+
+  useEffect(() => {
+    getResults()
+  }, [])
+
+  const getResults = async () => {
+    const response = await axios.get(`http://hn.algolia.com/api/v1/search?query=${query}`)
+    setResults(response.data.hits)
+  }
+
+  const handleSearch = event => {
+    event.preventDefault();
+    getResults();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSearch}>
+        <input type="text" onChange={event => setQuery(event.target.value)} value={query}  />
+        <button type="submit" onClick={getResults}>Search</button>
+      </form>
+        <ul>
+          {results.map(result => (
+            <li key={result.objectID}>
+              <a href={result.url}>
+                {result.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      
     </div>
   );
 }
-
-export default App;
